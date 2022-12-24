b6f82db41a26e25a7f34ed818184e35bceda6497
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-12-24 14:31:06 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -41,6 +41,21 @@ class App extends Component {
     );
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContact = JSON.parse(contacts);
+    if(parsedContact) {
+      this.setState({contacts: parsedContact})
+    }
+    
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if(this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  };
+
   render() {
     const { filter } = this.state;
     const addContact = this.addContact;
